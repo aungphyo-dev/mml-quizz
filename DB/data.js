add5a18a43da0404f46ff2161b9e003359bf3a86
typle <aungpyaephyo1412@gmail.com>
2023-11-01 15:06:56 +0630

    data

---
+++ DB/data.js
@@ -0,0 +1,67 @@
+const QUESTIONS = {
+  totalQuestions: 10,
+  questions: [
+    {
+      id: 1,
+      question: 'Question One Bla Bla?',
+      answers: ['Question One Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 0,
+    },
+    {
+      id: 2,
+      question: 'Question Two Bla Bla?',
+      answers: ['Question Two Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 3,
+    },
+    {
+      id: 3,
+      question: 'Question Three Bla Bla?',
+      answers: ['Question Three Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 0,
+    },
+    {
+      id: 4,
+      question: 'Question Four Bla Bla?',
+      answers: ['Question Four Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 2,
+    },
+    {
+      id: 5,
+      question: 'Question Five Bla Bla?',
+      answers: ['Question Five Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 3,
+    },
+    {
+      id: 6,
+      question: 'Question Six Bla Bla?',
+      answers: ['Question Six Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 2,
+    },
+    {
+      id: 7,
+      question: 'Question Seven Bla Bla?',
+      answers: ['Question Seven Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 3,
+    },
+    {
+      id: 8,
+      question: 'Question Eight Bla Bla?',
+      answers: ['Question Eight Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 1,
+    },
+    {
+      id: 9,
+      question: 'Question Nine Bla Bla?',
+      answers: ['Question Nine Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 3,
+    },
+    {
+      id: 10,
+      question: 'Question Ten Bla Bla?',
+      answers: ['Question Ten Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+      correctIndex: 0,
+    },
+  ]
+}
+
+export default QUESTIONS
